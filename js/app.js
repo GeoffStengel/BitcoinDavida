c89e7ec99368b9d1b9b53ae45b89fa334d7d4b87
GeoffStengel <geoffsglass@hotmail.com>
2022-11-09 05:41:25 +0200

    Add files via upload
     Updated kin.index for charts to read USD, changed bkgd color for info.css file, updated symbol on copper charts for better chart accuracy.  Added a different bkgd color to a few files to test

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
-var tl = new TimelineMax({repeat:-1, repeatDelay:0.1});
+var tl = new TimelineMax({ repeat: -1, repeatDelay: 0.1 });
 tl.staggerTo(".box, p", 1, {
   scale: 0.1,
-  y:60,
-  yoyo:true,
-  repeat:1,
-  ease:Power1.easeInOut,
+  y: 60,
+  yoyo: true,
+  repeat: 1,
+  ease: Power1.easeInOut,
   rotation: 360,
-stagger: .5
-
+  stagger: 0.5,
 });
-
-
-
-
 
 /****TweenMax.staggerFrom(".box, p", 0.5, {opacity: 0, y: 200, rotation: 360 }, 0.5); */
