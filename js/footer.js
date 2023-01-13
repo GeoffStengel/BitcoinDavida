c4b436dac87466e1b85b0a302438db4f89a9352e
GeoffStengel <geoffsglass@hotmail.com>
2023-01-13 04:06:42 +0200

    Add files via upload
     Updated Title in index.html and shop.html for more descriptive ones. also testing api for bitcoin prices (added then removed)

--- js/footer.js
+++ js/footer.js
@@ -6,10 +6,39 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 
 /*<!-- End Google Tag Manager for top of page-->\*/
 
+/**Test1**Bitcoin*API*Price*Coingeck0***Start**/
+/** 
+var btc = document.getElementById('bitcoin');
+
+var liveprice = {
+    "async": true,
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd",
+
+    "meth": "GET",
+    "headers": {}
+}
+
+$.ajax(liveprice).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+    console.log("Kinda Workin");
+});
+**/
+/*Test1***Bitcoin*API*Price*Coingeck0***Finish**/
 
 
+/*Test2***Bitcoin*API*Price*Coingeck0***Start**/
+/**
+var btc = document.getElementById('bitcoin');
 
 
+var  bitcoin = fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Sheats fcked Up somewhere'))
+console.log("Bitcoin Workin");
+**/
+/*Test2***Bitcoin*API*Price*Coingeck0***Finish**/
 
 
 
